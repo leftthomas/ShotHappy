6ccbd3fc1f4b711265d99f735c6dedc001121929
left <1223418167@qq.com>
2016-03-26 20:09:22 +0800

    重置密码界面、登录界面布局与逻辑实现

--- app/src/main/java/com/left/shothappy/LoginActivity.java
+++ app/src/main/java/com/left/shothappy/LoginActivity.java
@@ -5,13 +5,11 @@ import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.InputMethodManager;
@@ -20,6 +18,9 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import cn.bmob.v3.Bmob;
+import cn.bmob.v3.BmobUser;
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.LogInListener;
 
 
 /**
@@ -27,17 +28,13 @@ import cn.bmob.v3.Bmob;
  */
 public class LoginActivity extends AppCompatActivity {
 
-    /**
-     * Keep track of the login task to ensure we can cancel it if requested.
-     */
-    private UserLoginTask mAuthTask = null;
-
-    // UI references.
     private EditText mAccountView;
     private EditText mPasswordView;
     private View mProgressView;
     private View mLoginFormView;
     private TextView tvregister;
+    private TextView tvforget;
+    private Button mLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,21 +47,27 @@ public class LoginActivity extends AppCompatActivity {
 
         // Set up the login form.
         mAccountView = (EditText) findViewById(R.id.account);
-
         mPasswordView = (EditText) findViewById(R.id.password);
-
         tvregister = (TextView) findViewById(R.id.register);
+        tvforget = (TextView) findViewById(R.id.forget_password);
+        mLoginButton = (Button) findViewById(R.id.login_button);
+        mLoginFormView = findViewById(R.id.login_form);
+        mProgressView = findViewById(R.id.login_progress);
 
-        Button mAccountLoginButton = (Button) findViewById(R.id.login_button);
-        mAccountLoginButton.setOnClickListener(new OnClickListener() {
+        //利用本地缓存用户登录
+        BmobUser bmobUser = BmobUser.getCurrentUser(this);
+        if (bmobUser != null) {
+            //直接进入主页
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+
+        mLoginButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
                 attemptLogin();
             }
         });
-
-        mLoginFormView = findViewById(R.id.login_form);
-        mProgressView = findViewById(R.id.login_progress);
 
         tvregister.setOnClickListener(new OnClickListener() {
             @Override
@@ -73,17 +76,17 @@ public class LoginActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        tvforget.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), ForgetPasswordActivity.class));
+                finish();
+            }
+        });
     }
 
-    /**
-     * Attempts to login the account specified by the login form.
-     * If there are form errors (invalid account, missing fields, etc.), the
-     * errors are presented and no actual login attempt is made.
-     */
     private void attemptLogin() {
-        if (mAuthTask != null) {
-            return;
-        }
 
         // Reset errors.
         mAccountView.setError(null);
@@ -101,19 +104,11 @@ public class LoginActivity extends AppCompatActivity {
             mAccountView.setError(getString(R.string.error_field_required));
             focusView = mAccountView;
             cancel = true;
-        } else if (!isAccountValid(account)) {
-            mAccountView.setError(getString(R.string.error_invalid_email));
-            focusView = mAccountView;
-            cancel = true;
         }
 
         // Check for a valid password.
         else if (TextUtils.isEmpty(password)) {
             mPasswordView.setError(getString(R.string.error_field_required));
-            focusView = mPasswordView;
-            cancel = true;
-        } else if (!isPasswordValid(password)) {
-            mPasswordView.setError(getString(R.string.error_invalid_password));
             focusView = mPasswordView;
             cancel = true;
         }
@@ -126,21 +121,25 @@ public class LoginActivity extends AppCompatActivity {
             // Show a progress spinner, and kick off a background task to
             // perform the user dial attempt.
             showProgress(true);
-            mAuthTask = new UserLoginTask(account, password);
-            mAuthTask.execute((Void) null);
             //关闭键盘
             InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
             imm.hideSoftInputFromWindow(mPasswordView.getWindowToken(), 0);
+
+
+            BmobUser.loginByAccount(getApplicationContext(), account, password, new LogInListener<BmobUser>() {
+                @Override
+                public void done(BmobUser user, BmobException e) {
+                    if (user != null) {
+                        showProgress(false);
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        finish();
+                    } else {
+                        showProgress(false);
+                        Snackbar.make(mLoginFormView, getString(R.string.login_fail), Snackbar.LENGTH_SHORT).show();
+                    }
+                }
+            });
         }
-    }
-
-    private boolean isAccountValid(String email) {
-        return email.contains("@");
-    }
-
-    private boolean isPasswordValid(String password) {
-
-        return (password.length() == 6 && password.matches("[0-9]+"));
     }
 
     /**
@@ -179,59 +178,5 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
-    /**
-     * Represents an asynchronous login task used to authenticate
-     * the user.
-     */
-    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
-
-        private final String mAccount;
-        private final String mPassword;
-
-        UserLoginTask(String account, String password) {
-            mAccount = account;
-            mPassword = password;
-        }
-
-        @Override
-        protected Boolean doInBackground(Void... params) {
-
-            //模拟登录验证
-            try {
-                Thread.sleep(2000);
-                if(mPassword.equals("123456")){
-                    Log.d("login", "login success");
-                    return true;
-                }
-                else{
-                    Log.d("login", "login failed");
-                    return false;
-                }
-            } catch (Exception e) {
-                Log.d("login", "login error");
-                return false;
-            }
-
-        }
-
-        @Override
-        protected void onPostExecute(final Boolean success) {
-            mAuthTask = null;
-            showProgress(false);
-
-            if (success) {
-                startActivity(new Intent(getApplicationContext(),MainActivity.class));
-                finish();
-            } else {
-                Snackbar.make(mLoginFormView, getString(R.string.login_fail), Snackbar.LENGTH_SHORT).show();
-            }
-        }
-
-        @Override
-        protected void onCancelled() {
-            mAuthTask = null;
-            showProgress(false);
-        }
-    }
 }
 
