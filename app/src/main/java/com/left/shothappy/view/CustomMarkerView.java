50214f1c10aa33b95327d0928a71303f797cc2dd
left <1223418167@qq.com>
2016-03-30 18:04:51 +0800

    学习进度界面图表模拟绘制完成

---
+++ app/src/main/java/com/left/shothappy/view/CustomMarkerView.java
@@ -0,0 +1,36 @@
+package com.left.shothappy.view;
+
+import android.content.Context;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.utils.MarkerView;
+import com.left.shothappy.R;
+
+/**
+ * Created by left on 16/3/30.
+ */
+public class CustomMarkerView extends MarkerView {
+
+    private TextView tvContent;
+
+    public CustomMarkerView(Context context, int layoutResource) {
+        super(context, layoutResource);
+        tvContent = (TextView) findViewById(R.id.tvContent);
+    }
+
+    @Override
+    public void refreshContent(Entry e, int dataSetIndex) {
+        tvContent.setText("" + e.getVal());
+    }
+
+    @Override
+    public float getXOffset() {
+        return -(getWidth() / 2);
+    }
+
+    @Override
+    public float getYOffset() {
+        return -getHeight();
+    }
+}
--- app/src/main/java/com/left/shothappy/view/RateoflearningFragment.java
+++ app/src/main/java/com/left/shothappy/view/RateoflearningFragment.java
@@ -8,7 +8,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
@@ -24,42 +28,50 @@ import java.util.ArrayList;
  */
 public class RateoflearningFragment extends Fragment {
 
+    //进步曲线（总进度，统计历史量）
+    private LineChart mLineChart;
+    //每日学习量（近七天单日学习量）
+    private BarChart mBarChart;
+    // 自定义颜色
+    private int mLineColors = Color.rgb(137, 230, 81);
+    private int mBarColors = Color.rgb(240, 240, 30);
+    // 自定义字体
+    private Typeface mTf;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_rateoflearning, container, false);
-
+        mTf = Typeface.createFromAsset(getActivity().getAssets(), "OpenSans-Bold.ttf");
         return view;
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        LineChart mChart;
-        int mColors = Color.rgb(137, 230, 81); // 自定义颜色
-        mChart = (LineChart) view.findViewById(R.id.linechart);
+
+        mLineChart = (LineChart) view.findViewById(R.id.linechart);
+        mBarChart = (BarChart) view.findViewById(R.id.barchart);
         // 生产数据
-        LineData data = getData(36, 100);
-        setupChart(mChart, data, mColors);
+        LineData lineData = getLineData(36, 100);
+        BarData barData = getBarData(7, 100);
+        setupLineChart(mLineChart, lineData, mLineColors);
+        setupBarChart(mBarChart, barData, mBarColors);
+
     }
 
     // 设置显示的样式
-    void setupChart(final LineChart chart, LineData data, int color) {
-
-        Typeface mTf; // 自定义显示字体
-        // 自定义字体
-        mTf = Typeface.createFromAsset(getActivity().getAssets(), "OpenSans-Bold.ttf");
+    private void setupLineChart(final LineChart chart, LineData data, int color) {
 
         // disable the drawing of values into the chart
         chart.setDrawYValues(false);
-
         chart.setDrawBorder(false);
 
         //不显示数据描述
         chart.setDescription("");
 
         // 如果没有数据的时候，会显示这个，类似listview的emtpyview
-        chart.setNoDataTextDescription("没有数据呢(⊙o⊙)");
+        chart.setNoDataTextDescription("没有数据呢(⊙o⊙),快去学习吧");
 
         // enable / disable grid lines
         chart.setDrawVerticalGrid(false); // 是否显示垂直线
@@ -69,8 +81,7 @@ public class RateoflearningFragment extends Fragment {
         chart.setGridColor(Color.WHITE & 0x70FFFFFF); // 表格的颜色，在这里是给颜色设置一个透明度
         chart.setGridWidth(1.25f);// 表格线的线宽
 
-        // disable touch gestures
-        chart.setTouchEnabled(true); // 设置是否可以触摸
+        // 设置是否可以缩放
         chart.setDoubleTapToZoomEnabled(false);
         chart.setScaleEnabled(false);
 
@@ -79,6 +90,11 @@ public class RateoflearningFragment extends Fragment {
         chart.setValueTypeface(mTf);// 设置字体
 
         chart.setData(data); // 设置数据
+
+        CustomMarkerView mv = new CustomMarkerView(getActivity(), R.layout.custom_marker_view_layout);
+
+        // set the marker to the chart
+        chart.setMarkerView(mv);
 
         // get the legend (only possible after setting data)
         Legend l = chart.getLegend(); // 设置标示
@@ -103,8 +119,59 @@ public class RateoflearningFragment extends Fragment {
     }
 
 
-    // 生成一个数据，
-    LineData getData(int count, float range) {
+    // 设置显示的样式
+    private void setupBarChart(final BarChart chart, BarData data, int color) {
+
+        chart.setDrawBarShadow(false);
+        chart.setDrawBorder(false);
+        chart.setDrawValueAboveBar(true);
+
+        //不显示数据描述
+        chart.setDescription("");
+
+        // 如果没有数据的时候，会显示这个，类似listview的emtpyview
+        chart.setNoDataTextDescription("没有数据呢(⊙o⊙),快去学习吧");
+
+        // enable / disable grid lines
+        chart.setDrawVerticalGrid(false); // 是否显示垂直线
+
+        // enable / disable grid background
+        chart.setDrawGridBackground(false); // 是否显示表格颜色
+        chart.setGridColor(Color.WHITE & 0x70FFFFFF); // 表格的颜色，在这里是给颜色设置一个透明度
+        chart.setGridWidth(1.25f);// 表格线的线宽
+
+        // 设置是否可以缩放
+        chart.setDoubleTapToZoomEnabled(false);
+        chart.setScaleEnabled(false);
+        chart.setBackgroundColor(color);// 设置背景
+
+        chart.setValueTypeface(mTf);// 设置字体
+
+        chart.setData(data); // 设置数据
+        // get the legend (only possible after setting data)
+        Legend l = chart.getLegend(); // 设置标示
+
+        // modify the legend ...
+        // l.setPosition(LegendPosition.LEFT_OF_CHART);
+        l.setForm(Legend.LegendForm.CIRCLE);// 样式
+        l.setFormSize(6f);// 字体
+        l.setTextColor(Color.WHITE);// 颜色
+        l.setTypeface(mTf);// 字体
+
+        YLabels y = chart.getYLabels(); // y轴的标示
+        y.setTextColor(Color.WHITE);
+        y.setTypeface(mTf);
+
+        XLabels x = chart.getXLabels(); // x轴显示的标签
+        x.setTextColor(Color.WHITE);
+        x.setTypeface(mTf);
+
+        // animate calls invalidate()...
+        chart.animateY(3000); // 立即执行的动画,x轴
+    }
+
+    // 生成进步曲线数据
+    private LineData getLineData(int count, float range) {
         ArrayList<String> xVals = new ArrayList<>();
         for (int i = 0; i < count; i++) {
             // x轴显示的数据，这里默认使用数字下标显示
@@ -114,7 +181,7 @@ public class RateoflearningFragment extends Fragment {
         // y轴的数据
         ArrayList<Entry> yVals = new ArrayList<>();
         for (int i = 0; i < count; i++) {
-            float val = (float) (Math.random() * range) + 3;
+            float val = i;
             yVals.add(new Entry(val, i));
         }
 
@@ -133,6 +200,37 @@ public class RateoflearningFragment extends Fragment {
 
         // create a data object with the datasets
         LineData data = new LineData(xVals, dataSets);
+
+        return data;
+    }
+
+    // 生成每日学习量数据，
+    private BarData getBarData(int count, float range) {
+        //x轴的数据--日期（近七天）
+        ArrayList<String> xVals = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            xVals.add(i + "");
+        }
+
+
+        //y轴的数据--学习单词数（对应日期）
+        ArrayList<BarEntry> yVals = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            float val = (float) (Math.random() * range) + 3;
+            yVals.add(new BarEntry(val, i));
+        }
+
+        // y轴的数据集合
+        BarDataSet set = new BarDataSet(yVals, "每日学习量");
+
+        set.setColor(Color.GREEN);// 显示颜色
+        set.setHighLightColor(Color.BLUE);
+
+        ArrayList<BarDataSet> dataSets = new ArrayList<>();
+        dataSets.add(set); // add the datasets
+
+        // create a data object with the datasets
+        BarData data = new BarData(xVals, dataSets);
 
         return data;
     }
