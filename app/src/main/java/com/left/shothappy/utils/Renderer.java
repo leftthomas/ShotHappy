39be24df8033e7034fe104c9d10a6851c7ba24f6
left <1223418167@qq.com>
2016-04-05 12:33:36 +0800

    添加easyar

---
+++ app/src/main/java/com/left/shothappy/utils/Renderer.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2015-2016 VisionStar Information Technology (Shanghai) Co., Ltd. All Rights Reserved.
+ * EasyAR is the registered trademark or trademark of VisionStar Information Technology (Shanghai) Co., Ltd in China
+ * and other countries for the augmented reality technology developed by VisionStar Information Technology (Shanghai) Co., Ltd.
+ */
+
+package com.left.shothappy.utils;
+
+import android.opengl.GLSurfaceView;
+
+import com.left.shothappy.views.ARFragment;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class Renderer implements GLSurfaceView.Renderer {
+
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        ARFragment.nativeInitGL();
+    }
+
+    public void onSurfaceChanged(GL10 gl, int w, int h) {
+        ARFragment.nativeResizeGL(w, h);
+    }
+
+    public void onDrawFrame(GL10 gl) {
+        ARFragment.nativeRender();
+    }
+
+}
